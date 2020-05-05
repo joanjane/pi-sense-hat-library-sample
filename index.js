f133db2c9b7c0c6289693959b06a767fb21d5e19
Joan Jané Ballester <jjane@plainconcepts.com>
2020-05-05 13:57:27 +0200

    Add logs

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function App(display, joystick, environmentSensors, motionSensors) {
     // 'left' | 'up' | 'right' | 'down' | 'click'
     if (e === 'up') {
       environmentSensors.getSensorsStatus().then(envSensorsStatus => {
+        console.log(JSON.stringify(envSensorsStatus));
         display.showMessage('Temperature: ' + envSensorsStatus.temperature + ' C', 1, '#004400', () => {
           display.showMessage('Humidity: ' + envSensorsStatus.humidity + '%', 1, '#440000', () => {
             display.showMessage('Pressure: ' + envSensorsStatus.pressure + 'hPa', 1, '#000044');
@@ -42,20 +43,24 @@ function App(display, joystick, environmentSensors, motionSensors) {
       });
     } else if (e === 'right') {
       motionSensors.getMotionStatus().then((motionStatus) => {
+        console.log(JSON.stringify(motionStatus));
         const vector = motionStatus.acceleration;
         display.showMessage('Accel[x,y,z]: ' + vector[0] + '/' + vector[1] + '/' + vector[2], 0.2, '#aabbcc');
       });
     } else if (e === 'left') {
       motionSensors.getMotionStatus().then((motionStatus) => {
+        console.log(JSON.stringify(motionStatus));
         const vector = motionStatus.gyroscope;
         display.showMessage('Gyro[x,y,z]: ' + vector[0] + '/' + vector[1] + '/' + vector[2], 0.2, '#334455');
       });
     } else if (e === 'down') {
       motionSensors.getMotionStatus().then((motionStatus) => {
+        console.log(JSON.stringify(motionStatus));
         const vector = motionStatus.orientation;
         display.showMessage('Orient[x,y,z]: ' + vector[0] + '/' + vector[1] + '/' + vector[2], 0.2, '#aa3300');
       });
     } else if (e === 'click') {
+      console.log(JSON.stringify(motionStatus));
       motionSensors.getMotionStatus().then((motionStatus) => {
         display.showMessage('Compass: ' + motionStatus.compass + 'deg', 0.2, '#33aa00');
       });
